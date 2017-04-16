a02a440bcc00c6e973ca7b11f036202f36c178ca
Sai <ssm@gatech.edu>
2017-04-16 18:39:05 -0400

    playController.java - Sends scores to the server. This only happens *per song*, meaning it doesn't send the entire CSV file. The decision is, why would the server need to keep track of user's scores if they aren't registered? One issue with this is that if you don't have internet, your score would stay in the CSV file but never go to the server. This can be fixed with a second CSV file that contains scores that are pending to be sent up. scores.csv - The date/time is a little different to accommodate the database.

--- src/main/java/controllers/playController.java
+++ src/main/java/controllers/playController.java
@@ -29,6 +29,9 @@ import org.omg.CORBA.Current;
 
 import javax.sound.midi.*;
 import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -497,7 +500,7 @@ public class playController {
 
     @FXML
     public void saveScore(String name, String score, String rating) {
-        String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Timestamp(System.currentTimeMillis()));
+        String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Timestamp(System.currentTimeMillis()));
         File scoreFile = new File("scores.csv");
 
         FileWriter writer = null;
@@ -520,6 +523,54 @@ public class playController {
                 System.out.println("Writer closed");
             } catch (IOException e) {
                 e.printStackTrace();
+            }
+        }
+
+        if (Preludio.getInstance().getUserName() != "User") {
+            try {
+                URL url = new URL("http://ec2-54-214-225-63.us-west-2.compute.amazonaws.com/extern_send_score.php");
+                Map<String, Object> params = new HashMap<>();
+                params.put("username", Preludio.getInstance().getUserName());
+                params.put("songname", name);
+                params.put("score", score);
+                params.put("rating", rating);
+                params.put("date", timeStamp);
+
+                StringBuilder postData = new StringBuilder();
+                // POST as urlencoded is basically key-value pairs, as with GET
+                // This creates key=value&key=value&... pairs
+                for (Map.Entry<String,Object> param : params.entrySet()) {
+                    if (postData.length() != 0) postData.append('&');
+                    postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+                    postData.append('=');
+                    postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+                }
+
+                byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                // Tell server that this is POST and in which format is the data
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+                conn.setDoOutput(true);
+                conn.getOutputStream().write(postDataBytes);
+
+                // This gets the output from your server
+                Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+
+                String response = "";
+
+                for (int c; (c = in.read()) >= 0;)
+                    response += (char) c;
+
+                if (response.equals("Valid")) {
+                    System.out.println("Successfully sync'd score.");
+                } else if (response.equals("Invalid")){
+                    System.out.println("Couldn't successfully sync score.");
+                }
+            } catch (Exception mue) {
+                mue.printStackTrace();
             }
         }
     }
